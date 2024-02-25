5c1234f6f7fdc3923ad16e0528493eab0abec818
TSCarol <thaiscsantoos@gmail.com>
2024-02-24 22:26:54 -0300

    content created new

--- angular-blog/src/app/data/data.fake.ts
+++ angular-blog/src/app/data/data.fake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVA TECNOLOGIA NO MERCADO",
-    "description": "Na busca contínua por avanços tecnológicos, a computação quântica emerge como uma promissora fronteira, redefinindo os limites do que é possível no campo da informática. Diferentemente dos computadores tradicionais, que operam com bits binários (0 ou 1), os computadores quânticos utilizam qubits, unidades fundamentais de informação que podem representar simultaneamente 0, 1 ou qualquer superposição desses estados.",
-    "photoCover":"https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg"
+    "title": "NOVO FILME DO HOMEM ARANHA PODE SAIR EM 2026",
+    "description": "Após o sucesso estrondoso de Homem-Aranha: No Way Home, os fãs do universo cinematográfico da Marvel estão ansiosos por mais aventuras do amado super-herói aracnídeo. Recentemente, surgiram rumores excitantes sugerindo que uma nova produção do Homem-Aranha poderia estar nos planos para 2026. Embora nenhum anúncio oficial tenha sido feito pela Marvel Studios até o momento, fontes internas sugerem que a equipe criativa está explorando novas maneiras de expandir o universo do Homem-Aranha na tela grande. Com a introdução de múltiplos universos e a inclusão de personagens icônicos de diferentes realidades em No Way Home, os fãs especulam que o próximo filme poderia mergulhar ainda mais nesse conceito. Houve sugestões de que o filme poderia explorar a família do Homem-Aranha, trazendo à tona personagens como Mayday Parker, a filha do Peter Parker de uma realidade alternativa, ou mesmo uma abordagem totalmente nova e inesperada para o herói. Embora detalhes concretos sobre a trama, elenco e equipe criativa ainda sejam escassos, a especulação está alimentando a empolgação dos fãs, que mal podem esperar para ver o que o futuro reserva para o Homem-Aranha nas telonas. Fique ligado para mais atualizações e anúncios oficiais conforme continuamos a acompanhar de perto qualquer desenvolvimento relacionado ao possível filme do Homem-Aranha em 2026!",
+    "photoCover":"https://cdn.pixabay.com/photo/2014/11/03/13/33/spiderman-515215_1280.jpg"
   },
   {
     "id":"2",
-    "title": "Nova revista da Marvel vem com tudo para próxima edição",
-    "description": "ola",
-    "photoCover":"https://cdn.pixabay.com/photo/2020/06/27/13/02/comic-5345814_1280.jpg"
+    "title": "Veja todos os jogos que vão sair em 2024 e 2025",
+    "description": "Cameron retorna ao mundo de Pandora com uma sequência épica que expande ainda mais o universo de Avatar. Prepare-se para novos cenários deslumbrantes, criaturas fantásticas e uma história que cativará a imaginação de espectadores de todas as idades.Além desses filmes altamente antecipados, também foi confirmado que Meninas Malvadas: Reunião e Pennywise: A Origem estão em estágios avançados de desenvolvimento para lançamento em 2025, trazendo nostalgia e terror para as telonas.Com uma programação tão emocionante pela frente, os próximos anos prometem ser verdadeiramente inesquecíveis para os fãs de cinema em todo o mundo. Prepare-se para se emocionar, rir, chorar e se maravilhar com uma seleção de filmes que certamente deixarão uma marca duradoura na história do cinema.",
+    "photoCover":"https://cdn.pixabay.com/photo/2021/09/07/07/11/game-console-6603120_1280.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do Deadpool já tem data de estreia",
+    "description": "Os fãs do Mercenário Tagarela favorito de todos estão prestes a ter um motivo para comemorar, pois foi oficialmente confirmado que um novo filme do Deadpool está programado para chegar aos cinemas em 2024! Com uma mistura de ação, comédia e caos, este novo capítulo promete ser uma explosão de entretenimento como nenhum outro. Uma das maiores surpresas desta nova produção é o anúncio de um elenco renovado, trazendo uma nova perspectiva para o universo do Deadpool. Enquanto o carismático Ryan Reynolds continua a dar vida ao anti-herói com seu humor afiado e charme inegável, novos rostos se juntarão à franquia para adicionar uma nova camada de diversão e emoção. Além disso, os fãs ficarão encantados em saber que os X-Men farão uma participação especial neste novo filme. Depois de uma colaboração de sucesso no passado, Deadpool une forças mais uma vez com os membros mais famosos da equipe mutante. Prepare-se para momentos hilariantes e ação de tirar o fôlego enquanto Deadpool e os X-Men enfrentam desafios épicos juntos. Embora detalhes específicos sobre a trama ainda sejam mantidos em segredo, os rumores sugerem que este novo filme levará os personagens a novos patamares de loucura e aventura. Com o diretor prometendo uma abordagem fresca e inovadora, os fãs podem esperar por reviravoltas surpreendentes e momentos memoráveis que os manterão à beira de seus assentos. Com o retorno esperado de Deadpool e a inclusão emocionante dos X-Men, este filme promete ser uma experiência cinematográfica imperdível para fãs de todas as idades. Preparem-se para um turbilhão de risadas, ação e diversão quando Deadpool voltar às telonas em 2024.",
+    "photoCover":"https://cdn.pixabay.com/photo/2019/03/21/02/51/deadpool-4070071_1280.jpg"
+  },
+  {
+    "id":"4",
+    "title": "O novo óculos de realidade da Apple- Vision Pro",
+    "description": "A Apple anunciou seu mais recente lançamento, o Vision Pro, um avançado óculos de realidade que promete uma experiência imersiva e única. Combinando realidade aumentada e virtual, o Vision Pro oferece uma resolução ultra-alta, rastreamento preciso de movimento e áudio espacial envolvente. Integra-se perfeitamente com outros dispositivos Apple, prometendo uma experiência de usuário fluida e contínua. Fique atento para mais informações sobre o lançamento do Vision Pro.",
+    "photoCover":"https://cdn.pixabay.com/photo/2016/10/11/09/26/office-1730939_1280.jpg"
   }
 ]
